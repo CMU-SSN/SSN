f0d43bcc0c5df32a383b0bea917ae20e53fd8e9b
Jason Leng <cool_curly@hotmail.com>
2013-02-19 11:38:41 -0800

    Prevent user from posting empty messages.

--- social-network-engine/app/models/post.rb
+++ social-network-engine/app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ActiveRecord::Base
   attr_accessible :text
   belongs_to :user
+  validates :text, :length => {
+      :minimum   => 1,
+      :maximum   => 140,
+      :tokenizer => lambda { |str| str.scan(/\w+/) }
+  }
 end
