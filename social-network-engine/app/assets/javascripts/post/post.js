cfa311b20e44efb9010a61e2cda349dc12a8d153
Jason Leng <cool_curly@hotmail.com>
2013-02-23 15:12:05 -0800

    Added geolocation support.

--- social-network-engine/app/assets/javascripts/post/post.js
+++ social-network-engine/app/assets/javascripts/post/post.js
@@ -1,5 +1,6 @@
 $('document').ready(function () {
 
+    // auto refresh posts
     setInterval(function () {
         var token = $('#posts').data('token');
         $.ajax({
@@ -13,8 +14,7 @@ $('document').ready(function () {
             },
             success: function (data) {
                 if (data.code === 200) {
-                    if (data.token)
-                    {
+                    if (data.token) {
                         $('#posts').data('token', data.token);
                     }
 
@@ -35,4 +35,30 @@ $('document').ready(function () {
             }
         });
     }, 10000);
+
+    // intercept post button clicking event
+    $("body").delegate("#submit-post", "click", function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                    if (position && position.coords && position.coords.latitude && position.coords.longitude) {
+                        $("#post_latitude").val(position.coords.latitude);
+                        $("#post_longitude").val(position.coords.longitude);
+                    }
+
+                    $("#new_post").submit();
+                }, function (err) {
+                    $("#new_post").submit();
+                },
+                {
+                    timeout: 3000
+                });
+
+            return;
+        }
+
+        $("#new_post").submit();
+    });
 });
--- social-network-engine/app/models/post.rb
+++ social-network-engine/app/models/post.rb
@@ -1,9 +1,18 @@
 class Post < ActiveRecord::Base
-  attr_accessible :text
+  attr_accessible :text, :address, :latitude, :longitude
   belongs_to :user
   validates :text, :length => {
       :minimum   => 1,
       :maximum   => 140,
       :tokenizer => lambda { |str| str.scan(/\w+/) }
   }
+  geocoded_by :address
+  after_validation :geocode
+  reverse_geocoded_by :latitude, :longitude do |obj,results|
+    geo = results.first
+    unless geo.nil?
+      obj.city = geo.city
+    end
+  end
+  after_validation :reverse_geocode
 end
