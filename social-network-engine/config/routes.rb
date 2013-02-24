83d3e7364dc42d150cc0d0d14eb1f945d02d2098
Victor Marmol <vmarmol@gmail.com>
2013-02-23 18:03:52 -0800

    Adding base of search controller and page. Also moved Sign Out and Search buttons into the application template since those will always be there

--- social-network-engine/config/routes.rb
+++ social-network-engine/config/routes.rb
@@ -6,6 +6,8 @@ Sne::Application.routes.draw do
   put "facebook_tab_app/create_account"
   get "facebook_tab_app/done"
 
+  match '/search' => 'search#search'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   match 'main' => 'posts#index'
