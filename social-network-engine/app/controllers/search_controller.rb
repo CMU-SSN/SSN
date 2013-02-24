e9b18346a1236184348cd7fa2b44236328b6a63d
Victor Marmol <vmarmol@gmail.com>
2013-02-23 22:58:32 -0800

    Search page which only looks through users for now

--- social-network-engine/app/controllers/search_controller.rb
+++ social-network-engine/app/controllers/search_controller.rb
@@ -1,5 +1,18 @@
 class SearchController < ApplicationController
   def search
-    print 'here at search'
+    @query = params[:q]
+    @results = nil
+
+    print "\n\n\nSearch |"+@query.to_s+"|\n\n\n"
+
+    # Search if a query was specified
+    if not @query.nil? and not @query.strip().empty?
+      @result = []
+
+      # Search users
+      user_query = "%#{@query}%"
+      @results = User.find(:all, :limit => 50, :conditions => ["name LIKE ?", user_query])
+      @results.map!{|u| { :type => "user", :user => u }}
+    end
   end
 end
