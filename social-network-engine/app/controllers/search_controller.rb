f79b45cf979b6cf6912f34d1ba6b1014698695d8
Victor Marmol <vmarmol@gmail.com>
2013-02-25 23:09:52 -0800

    Slightly smarter paging

--- social-network-engine/app/controllers/search_controller.rb
+++ social-network-engine/app/controllers/search_controller.rb
@@ -23,18 +23,20 @@ class SearchController < ApplicationController
     if not @query.nil? and not @query.strip().empty?
       @results = []
       offset = @page_num.to_i * SearchResult::PAGE_SIZE.to_i
+      # Fetch one more (which we won't displays so that we know if we can page)
+      limit = SearchResult::PAGE_SIZE.to_i + 1
 
       # Search posts
       if @type.to_i == SearchResult::Type::POST.to_i
         post_query = "%#{@query}%"
-        @results = Post.find(:all, :limit => SearchResult::PAGE_SIZE, :conditions => ["text LIKE ?", post_query], :order => "created_at DESC", :offset => offset)
+        @results = Post.find(:all, :limit => limit, :conditions => ["text LIKE ?", post_query], :order => "created_at DESC", :offset => offset)
         @results.map!{|p| SearchResult::CreatePostResult(p)}
       end
 
       # Search users
       if @type.to_i == SearchResult::Type::USER.to_i
         user_query = "%#{@query}%"
-        @results = User.find(:all, :limit => SearchResult::PAGE_SIZE, :conditions => ["name LIKE ?", user_query], :offset => offset)
+        @results = User.find(:all, :limit => limit, :conditions => ["name LIKE ?", user_query], :offset => offset)
         @results.map!{|u| SearchResult::CreateUserResult(u)}
       end
 
