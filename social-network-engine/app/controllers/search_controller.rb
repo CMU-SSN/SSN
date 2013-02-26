ef4714931460f12724e4d3c869d739f2082a99cf
Victor Marmol <vmarmol@gmail.com>
2013-02-25 22:17:51 -0800

    Adding search by Posts and made it capable of searching by organization too

--- social-network-engine/app/controllers/search_controller.rb
+++ social-network-engine/app/controllers/search_controller.rb
@@ -3,18 +3,36 @@ class SearchController < ApplicationController
 
   def search
     @query = params[:q]
+    @type = params[:type]
     @results = nil
 
-    print "\n\n\nSearch |"+@query.to_s+"|\n\n\n"
+    # Default to posts
+    if @type.nil? or @type.empty?
+      @type = SearchResult::Type::POST
+    end
 
     # Search if a query was specified
     if not @query.nil? and not @query.strip().empty?
-      @result = []
+      @results = []
+
+      # Search posts
+      if @type.to_i == SearchResult::Type::POST.to_i
+        post_query = "%#{@query}%"
+        @results = Post.find(:all, :limit => 50, :conditions => ["text LIKE ?", post_query], :order => "created_at DESC")
+        @results.map!{|p| SearchResult::CreatePostResult(p)}
+      end
 
       # Search users
-      user_query = "%#{@query}%"
-      @results = User.find(:all, :limit => 50, :conditions => ["name LIKE ?", user_query])
-      @results.map!{|u| SearchResult::CreateUserResult(u)}
+      if @type.to_i == SearchResult::Type::USER.to_i
+        user_query = "%#{@query}%"
+        @results = User.find(:all, :limit => 50, :conditions => ["name LIKE ?", user_query])
+        @results.map!{|u| SearchResult::CreateUserResult(u)}
+      end
+
+      # Search organizations
+      if @type.to_i == SearchResult::Type::ORGANIZATION.to_i
+        # TODO: Search when they exist
+      end
     end
   end
 end
--- social-network-engine/app/helpers/search_helper.rb
+++ social-network-engine/app/helpers/search_helper.rb
@@ -1,9 +1,30 @@
 module SearchHelper
 end
 
-class SearchResult < Object
+module SearchResult
   module Type
-    USER = 1
+    POST = 1
+    USER = 2
+    ORGANIZATION = 3
+
+    def self.ToName(type)
+      if type == POST
+        "Post"
+      elsif type == USER
+        "User"
+      else
+        "Organization"
+      end
+    end
+  end
+
+  def self.CreatePostResult(post)
+    result = {
+      :type => Type::POST,
+      :post => post,
+    }
+
+    result
   end
 
   def self.CreateUserResult(user)
@@ -13,5 +34,17 @@ class SearchResult < Object
     }
 
     result
+  end
+
+  # Outputs a link tag which is active if the current_type is the active_type
+  def self.GetTypeLink(view, active_type, current_type, query)
+    active_class = ''
+    if active_type.to_i == current_type.to_i
+      active_class = 'ui-btn-active'
+    end
+
+    print "Got: " + active_class.to_s
+    type_link = view.search_path + "?type=" + current_type.to_s + "&q=" + query.to_s
+    view.link_to Type::ToName(current_type), type_link, :class => active_class
   end
 end
