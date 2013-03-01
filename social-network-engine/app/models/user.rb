12d2eca510dad120661602a71e38fa37b5df7490
Victor Marmol <vmarmol@gmail.com>
2013-02-28 20:45:11 -0800

    Remove support for potential friends since we are moving to a synching model

--- social-network-engine/app/models/user.rb
+++ social-network-engine/app/models/user.rb
@@ -8,7 +8,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :profile_pic, :email, :password, :password_confirmation, :remember_me, :token_expiration, :provider, :uid, :token
   has_many :posts, :dependent => :destroy
-  has_many :potential_friends
   has_many :friends
 
   def ImportFriends(facebook_friend_ids)
@@ -17,24 +16,9 @@ class User < ActiveRecord::Base
       # TODO(vmarmol): Do all these lookups at once.
       potential_friend = User.find_by_uid(i)
 
-      # Unknown friends are potential friends, known users are friends
-      if potential_friend.nil?
-        self.potential_friends << PotentialFriend.new(:user => self,
-            :friend_facebook_id => i)
-      else
+      # Add friends that exist
+      if not potential_friend.nil?
         self.friends << Friend.new(:user =>self, :friend => potential_friend)
-      end
-    end
-
-    # Look for self in PotentialFriends and turn into a Friend.
-    found_friends = PotentialFriend.find(:all,
-        :conditions => ["friend_facebook_id =?", self.uid])
-    if not found_friends.nil?
-      # Move from PotentialFriend to Friend
-      found_friends.each do |i|
-        i.user.friends << Friend.new(:user => i.user, :friend => self)
-        PotentialFriend.destroy_all(:user_id => i.user.id,
-            :friend_facebook_id => self.uid)
       end
     end
   end
