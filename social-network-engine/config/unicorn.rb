fca7934fdd0c33cd49262734816ec3ce18a27181
Jason Leng <cool_curly@hotmail.com>
2013-02-28 22:21:14 -0800

    Several updates for deployment scripts.

--- social-network-engine/config/unicorn.rb
+++ social-network-engine/config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen 5000, :tcp_nopush => true
 worker_processes 2
 timeout 30
