bd98d0a01281406b1dc9cef3336ee38dd9a79a0c
Victor Marmol <vmarmol@gmail.com>
2013-03-03 22:31:11 -0800

    Adding a query to get all cities. Adding a query to filter posts by those that a user is interested in

--- social-network-engine/app/models/organization.rb
+++ social-network-engine/app/models/organization.rb
@@ -6,4 +6,9 @@ class Organization < ActiveRecord::Base
 
   # The users who can manage this organization
   has_and_belongs_to_many :managers, :class_name => "User", :foreign_key => "organization_id", :association_foreign_key => "user_id", :join_table => "organization_administrators"
+
+  # Return all cities
+  def self.AllCities
+    Organization.find(:all, :conditions => ["is_city = ?", true])
+  end
 end
--- social-network-engine/app/models/post.rb
+++ social-network-engine/app/models/post.rb
@@ -16,15 +16,30 @@ class Post < ActiveRecord::Base
     end
   end
   after_validation :reverse_geocode
-  
+
   GOOD_STATUS = "Good"
   HELP_STATUS = "Help"
   IMMEDIATE_HELP_STATUS = "Need Immediate Help"
   ALL_STATUSES = [GOOD_STATUS, HELP_STATUS, IMMEDIATE_HELP_STATUS] 
- 
+
   validates_inclusion_of :text, :in => ALL_STATUSES, :if => :status?
-  
+
   def status?
     status == true
+  end
+
+  # Filters all posts to those this user is interested in. Also limits the posts
+  # returned and allows the specification of an offset.
+  def self.Filter(user, limit, offset)
+    # Get the IDs of all organizations the user is following
+    org_ids = user.organizations.map{|o| o.id}
+
+    # Add the IDs of the cities since all cities are organizations
+    org_ids += Organization::AllCities().map{|c| c.id}
+
+    # Get the IDs of all friends
+    friend_ids = user.friends.map{|f| f.id}
+
+    Post.find(:all, :conditions => ["user_id IN (?) or organization_id IN (?)", friend_ids, org_ids], :limit => limit, :offset => offset, :order => "created_at DESC")
   end
 end
