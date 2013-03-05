613d34f2303aeb6e5bd591adfdc739dc3c24e292
Brian Bailey <baileyb@gmail.com>
2013-03-04 21:55:10 -0800

    Added the check-in dialog and moved the js to the posts.js file

--- social-network-engine/app/assets/javascripts/post/post.js
+++ social-network-engine/app/assets/javascripts/post/post.js
@@ -1,4 +1,4 @@
-$('document').ready(function () {
+$(document).ready(function () {
 
     $("body").delegate(".update-status", "click", function() {
 		$("#post_status").val(true);
@@ -42,7 +42,7 @@ $('document').ready(function () {
     }, 10000);
 
     // intercept post button clicking event
-    $("body").delegate("#submit-post", "click", submitPost(e) );
+    $("body").delegate("#submitNewPost", "click", submitPost(e) );
 	
 	function submitPost(e){
         e.preventDefault();
@@ -86,3 +86,118 @@ $('document').ready(function () {
         $("#new_post").submit();
     }
 });
+
+$(document).bind("pagechange", function(toPage) {
+	clearBindings();
+	createBindings();
+	updatePickerText();
+	hidePicker();
+});
+
+$("#newPostDialog").on('pagebeforeshow', function() {
+	clearFormValues();
+});
+
+function createBindings() {
+	// Handlers for the organization picker
+	$("#postAsSwitch").click(function() {
+		if ($("#organizationPicker").is(":visible")) { hidePicker(); }
+		else { showPicker(); }
+	});
+	
+	$("#organizationPicker").find("input[name='post_as']").click(function() {
+		if (this.value == "self") {
+			$("#postAsBar").removeClass("gray");
+			$("#postAsBar").addClass("orange");
+			hidePicker();
+		} else {
+			$("#postAsBar").removeClass("orange");
+			$("#postAsBar").addClass("gray");
+			hidePicker();
+		}		
+		var organization = $("label[for='"+$(this).attr('id')+"']");
+		$("#postAsName").text("Posting as " + organization.text());
+		
+		$.ajax({
+			url: '/context',
+			data: { id: $(this).attr('id') },
+			dataType: 'json'
+		});
+		
+		//$("input[name='post[organization_id]']").val($(this).attr('id'));
+	});
+	
+	$("#submitNewPost").click(function() {
+		$("#post_text").val($("#new_post_text").val());
+		$("#new_post").submit();
+	});
+
+	// Handlers for the check-in dialog
+	$("#submitAllClear").click(function() {
+		$("#post_text").val("STATUS_OK");
+		$("#post_status").val("true");
+		$("#new_post").submit();
+	});		
+	
+	$("#submitNeedAssistance").click(function() {
+		$("#post_text").val("STATUS_NEEDS_ASSISTANCE");
+		$("#post_status").val("true");
+		$("#new_post").submit();
+		
+	});
+	
+	$("#submitNeedHelp").click(function() {
+		$("#post_text").val("STATUS_NEEDS_HELP");
+		$("#post_status").val("true");
+		$("#new_post").submit();
+	});
+}
+
+function clearBindings() {
+	$("#postAsSwitch").unbind('click');	
+	$("#organizationPicker").find("input[name='post_as']").unbind('click');	
+	$("#submitNewPost").unbind('click');
+	$("#submitAllClear").unbind('click');	
+	$("#submitNeedAssistance").unbind('click');	
+	$("#submitNeedHelp").unbind('click');	
+};
+
+function showPicker() {
+	$("#organizationPicker").show();
+	$("#postAsSwitch").find(".ui-icon").removeClass("ui-icon-arrow-d");
+	$("#postAsSwitch").find(".ui-icon").addClass("ui-icon-arrow-u");	
+};
+
+function hidePicker() {
+	$("#organizationPicker").hide();
+	$("#postAsSwitch").find(".ui-icon").removeClass("ui-icon-arrow-u");
+	$("#postAsSwitch").find(".ui-icon").addClass("ui-icon-arrow-d");
+};
+
+function updatePickerText() {
+	var id = $("input[checked='checked']").attr("id");
+	var organization = $("label[for='" + id + "']");
+	$("#postAsName").text("Posting as " + organization.text());
+	
+	console.log(id + " : " + organization);
+	if (id == "self") {
+		$("#postAsBar").removeClass("gray");
+		$("#postAsBar").addClass("orange");
+		hidePicker();
+	} else {
+		$("#postAsBar").removeClass("orange");
+		$("#postAsBar").addClass("gray");
+		hidePicker();
+	}
+};
+
+// Dialog Helper Methods
+function clearFormValues() {
+	$("#post_latitude").val('');
+	$("#post_longitude").val('');
+	$("#post_text").val('');
+	$("#post_status").val('');
+	
+	// Clear the text boxes
+	$("#new_post_text").val('');
+};
