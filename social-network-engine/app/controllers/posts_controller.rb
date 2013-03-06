5a625a2f0af1e276cac33fe4acac143c41147071
Brian Bailey <baileyb@gmail.com>
2013-03-06 15:11:22 -0800

    Removed unused actions from posts controller

--- social-network-engine/app/controllers/posts_controller.rb
+++ social-network-engine/app/controllers/posts_controller.rb
@@ -27,18 +27,7 @@ class PostsController < ApplicationController
       format.html {render :partial=>"partials/refresh"}
     end
   end
-
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @post }
-    end
-  end
-
+  
   # GET /posts/new
   # GET /posts/new.json
   def new
@@ -48,11 +37,6 @@ class PostsController < ApplicationController
       format.html # new.html.erb
       format.json { render json: @post }
     end
-  end
-
-  # GET /posts/1/edit
-  def edit
-    @post = Post.find(params[:id])
   end
   
   # GET /post_contect?id=
@@ -80,34 +64,6 @@ class PostsController < ApplicationController
       flash[:notice] = "Post created successfully!"
       format.html { redirect_to '/posts#index', notice: 'Post was successfully created.' }
       format.json { render json: @post, status: :created, location: @post }
-    end
-  end
-
-  # PUT /posts/1
-  # PUT /posts/1.json
-  def update
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
     end
   end
 end
