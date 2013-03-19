ccc9b7a93b7bb21f6efbe7368fe3000fa4ed92de
Jason Leng <cool_curly@hotmail.com>
2013-03-19 12:01:14 -0700

    Made posts autorefersh again.

--- social-network-engine/app/assets/javascripts/post/refresh.js
+++ social-network-engine/app/assets/javascripts/post/refresh.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-    $("body").delegate(".update-status", "click", function() {
-		$("#post_status").val(true);
-		$("#post_text").val($(this).attr("value"));
-        $("#new_post").submit();
-	});
     // auto refresh posts
     setInterval(function () {
         var token = $('#posts').data('token');
