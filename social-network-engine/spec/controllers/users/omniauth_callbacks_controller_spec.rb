b4d13cbc798cf58111160373f7b010da89dfd0d3
Victor Marmol <vmarmol@gmail.com>
2013-03-20 11:19:42 -0700

    Skeleton of omniauth_callback_controller

---
+++ social-network-engine/spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Users::OmniauthCallbacksController do
+  render_views
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  it "cosa" do
+    @request.env["omniauth.auth"] = { :provider => "facebook", :uid => "1"}
+    post :facebook
+    response.should be_success
+  end
+end
