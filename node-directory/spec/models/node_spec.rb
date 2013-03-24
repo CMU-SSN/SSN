bf3fbc3e9a6846775444244fd01b5d04acf4809f
Victor Marmol <vmarmol@gmail.com>
2013-03-24 22:31:50 +0000

    Tests for Node model

--- node-directory/spec/models/node_spec.rb
+++ node-directory/spec/models/node_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe Node do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :name => "Node Name",
+      :latitude => 10,
+      :longitude => 20,
+      :link => "ssn.com/signup",
+      :uid => "UID",
+      :checkin => DateTime.now 
+    }
+  end
+
+  it "should be successful with all the attributes" do
+    Node.create!(@attr)
+  end
+
+  it "should need a name" do
+    new_node = Node.new(@attr.merge(:name => ""))
+    new_node.should_not be_valid
+  end
+
+  it "should need a latitude" do
+    new_node = Node.new(@attr.merge(:latitude => nil))
+    new_node.should_not be_valid
+  end
+
+  it "should need a longitude" do
+    new_node = Node.new(@attr.merge(:longitude => nil))
+    new_node.should_not be_valid
+  end
+
+  it "should need a link" do
+    new_node = Node.new(@attr.merge(:link => ""))
+    new_node.should_not be_valid
+  end
+
+  it "should need a uid" do
+    new_node = Node.new(@attr.merge(:uid => ""))
+    new_node.should_not be_valid
+  end
+
+  it "should need a checkin" do
+    new_node = Node.new(@attr.merge(:checkin => nil))
+    new_node.should_not be_valid
+  end
 end
