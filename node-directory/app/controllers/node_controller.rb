8400111c542d65e8050e8a345ed7e0f48381a8e3
Victor Marmol <vmarmol@gmail.com>
2013-03-24 21:52:12 +0000

    Adding the ability to checkin to the directory server and update the registration

--- node-directory/app/controllers/node_controller.rb
+++ node-directory/app/controllers/node_controller.rb
@@ -1,23 +1,61 @@
 class NodeController < ApplicationController
+
+  # Registers a node and allows existing nodes to update their registration.
+  # On a successful registration it outputs a random UID for the node. The node
+  # can then use that UID to update its entry.
+  #
+  # To create an entry:
+  #
+  #   /register?name=<NAME>&latitude=<LATITUDE>&longitude=<LONGITUDE>&hostname=<HOSTNAME>
+  #
+  # To update an entry:
+  #
+  #   /register?uid=<UID>&<any params that you want to update>
   def register
     link =  params[:hostname]
     if not link.nil?
       link += "/signup"
     end
 
-    node = Node.new(
-        :name =>  params[:name],
-        :latitude => params[:latitude],
-        :longitude => params[:longitude],
-        :link => link)
+    uid = params[:uid]
+    if uid.nil? or uid.empty?
+      # New node
+      node = Node.new(
+          :name =>  params[:name],
+          :latitude => params[:latitude],
+          :longitude => params[:longitude],
+          :link => link,
+          :uid => SecureRandom.urlsafe_base64(20),
+          :checkin => DateTime.now)
+    else
+      # Old node, get and update
+      node = Node.find_by_uid(uid)
+      if node.nil?
+        render :json => { :success => false, :errors => ["Unknown UID"]}
+      end
+
+      # Update name, lat, long, and link only
+      attrs = {}
+      attrs[:name] = params[:name]
+      attrs[:latitude] = params[:latitude]
+      attrs[:longitude] = params[:longitude]
+      attrs[:link] = params[:hostname] + "/signup" unless  params[:hostname].nil?
+
+      # Register the checkin
+      attrs[:checkin] = DateTime.now
+      node.update_attributes(attrs)
+    end
 
     if node.save
-      render :json => { :success => true }
+      render :json => { :success => true, :uid => node.uid }
     else
       render :json => { :success => false, :errors => node.errors }
     end
   end
 
+  # Searches for nodes that are within 5 miles of the specified query location.
+  # Implements paging for the results.
+  # TODO(vmarmol): Downrank those that have not been updated in a while
   def search
     @query = params[:q]
     @page_num = params[:page]
--- node-directory/app/models/node.rb
+++ node-directory/app/models/node.rb
@@ -1,10 +1,12 @@
 class Node < ActiveRecord::Base
-  attr_accessible :address, :city, :state, :latitude, :longitude, :name, :zipcode, :link
+  attr_accessible :address, :city, :state, :latitude, :longitude, :name, :zipcode, :link, :uid, :checkin
 
   validates :name, :presence => true
   validates :latitude, :presence => true
   validates :longitude, :presence => true
   validates :link, :presence => true
+  validates :uid, :presence => true
+  validates :checkin, :presence => true
 
   geocoded_by :address
   after_validation :geocode
