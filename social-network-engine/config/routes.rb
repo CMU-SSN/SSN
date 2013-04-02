9415e634f631a61b36ed04ef03968e2066a7874b
Victor Marmol <vmarmol@gmail.com>
2013-04-02 05:17:23 +0000

    Adding /refresh which grabs FB info for all users.

--- social-network-engine/config/routes.rb
+++ social-network-engine/config/routes.rb
@@ -4,6 +4,7 @@ Sne::Application.routes.draw do
 	get 'context' => 'posts#post_context'
   get 'checkin' => 'posts#checkin'
   get 'signup' => 'facebook_tab_app#signup'
+  get 'refresh' => 'facebook_tab_app#refresh'
 
   # All facebook_tab_app actions just get forwarded
   match ':controller/:action', :controller => /facebook_tab_app/
