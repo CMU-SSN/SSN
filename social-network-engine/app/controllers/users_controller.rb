16b2cccafa000d89457821ea35037cd06de2536d
James Ricks <james.ricks@sv.cmu.edu>
2013-04-09 02:14:17 -0600

    Added subscription logic for users and organizations Changed structure to a follow/follower profile model Changed profile to ajax to accomodate subscriptions

--- social-network-engine/app/controllers/users_controller.rb
+++ social-network-engine/app/controllers/users_controller.rb
@@ -3,11 +3,65 @@ class UsersController < ApplicationController
   def show
 		@user = User.find(params[:id])
 		@posts = @user.posts.reverse
-		@posts.delete_if {|post| post.organization != nil}
+
+		#user.posts also contains posts that the users associated organization posted
+		#remove organization posts
+		@posts.delete_if {|post| post.organization != nil} 
 		
 		@path = "../"
     respond_to do |format|
       format.html # show.html.erb
     end
   end
+	
+  # POST followUser/?id=
+  def add_friend
+		user = User.find(params[:id])
+		if !current_user.friends.include?(user)	
+			current_user.friends << User.find(params[:id])
+		end
+		respond_to do |format|
+      format.json { head :ok }
+    end
+  end
+	
+	# POST removeUser/?id=
+  def remove_friend
+		user = User.find(params[:id])
+		if current_user.friends.include?(user)	
+			current_user.friends.delete(User.find(params[:id]))
+		end
+		respond_to do |format|
+      format.json { head :ok }
+    end
+  end
+	
+	def followings
+		@user = User.find(params[:id])
+		@path = "../"
+		@feedItemId = "followingFeedItemId"
+	end
+	
+	def followers
+		@user = User.find(params[:id])
+		@path = "../"
+	end
+	
+	def posts
+		@user = User.find(params[:id])
+		@posts = @user.posts.reverse
+		@path = "../"
+
+		#user.posts also contains posts that the users associated organization posted
+		#remove organization posts
+		@posts.delete_if {|post| post.organization != nil} 
+	end
+	
+	def organizations
+		@user = User.find(params[:id])
+		@path = "../"
+		if(current_user == @user)
+			@feedItemId = "followingFeedItemId"
+		end
+	end
 end
