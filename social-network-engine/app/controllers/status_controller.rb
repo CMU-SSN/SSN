66579975fdb4519612d8b9165b6f854d20456def
Jason Leng <cool_curly@hotmail.com>
2013-04-29 00:07:30 -0700

    Sort statuses by location with fallbacks to severity if location is not available

--- social-network-engine/app/controllers/status_controller.rb
+++ social-network-engine/app/controllers/status_controller.rb
@@ -1,16 +1,24 @@
 class StatusController < ApplicationController
   before_filter :authenticate_user!
   def index
+    lat, lng = params[:latitude], params[:longitude]
+
     @status = current_user.status
-    @statuses = Status.includes(:user).where("status in (?)", [Post::NEEDS_ASSISTANCE_STATUS, Post::NEEDS_HELP_STATUS]).order("status DESC").map do |status|
+    @statuses = Status.includes(:user).order("severity DESC").map do |status|
       status.clear = true if status.user.id == current_user.id
       status
+    end
+    if lat && lng
+      @statuses = @statuses.sort_by do  |a|
+        Geocoder::Calculations.distance_between([a.latitude,a.longitude], [lat,lng])
+      end
     end
     @path = "../"
   end
 
   def create
     @status = params[:status]
+
     unless current_user.status.nil?
       current_user.status.update_attributes(@status)
     else
@@ -18,7 +26,7 @@ class StatusController < ApplicationController
     end
 
     respond_to do |format|
-      format.html {redirect_to status_path}
+      format.html {redirect_to status_path(:latitude => @status[:latitude], :longitude => @status[:longitude])}
     end
   end
 
--- social-network-engine/app/models/status.rb
+++ social-network-engine/app/models/status.rb
@@ -2,4 +2,15 @@ class Status < ActiveRecord::Base
   attr_accessible :status, :latitude, :longitude
   attr_accessor :clear
   belongs_to :user
+  before_save :set_severity
+  private
+  def set_severity
+    if (status == Post::ALL_CLEAR_STATUS)
+      @severity = 0
+    elsif (status == Post::NEEDS_ASSISTANCE_STATUS)
+      @severity = 1
+    else
+      @severity = 2
+    end
+  end
 end
