bc21d859b1d398dee1d8b98d18244d0d9564984a
yukinishida <glory791days@gmail.com>
2013-09-28 14:08:53 -0700

    Fixed menu bar to all pages for signed-in users

--- social-network-engine/bootstrap.sh
+++ social-network-engine/bootstrap.sh
@@ -9,12 +9,12 @@ apt-get install -y git
 
 # Install RVM if required
 if [ ! -e /usr/local/rvm/bin/rvm ]
-	then
-		echo "Installing RVM..."
-		curl -L https://get.rvm.io | bash -s stable
-		source /etc/profile.d/rvm.sh
-		rvm autolibs enable
-		rvm install 1.9.3-p385
+    then
+        echo "Installing RVM..."
+        curl -L https://get.rvm.io | bash -s stable
+        source /etc/profile.d/rvm.sh
+        rvm autolibs enable
+        rvm install 1.9.3-p385
 fi
 
 # Load RVM into the shell
