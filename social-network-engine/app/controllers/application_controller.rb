c04a94d025bcb1613b94e1c013479a7bccb69833
Xinfeng Le <xinfeng.le@gmail.com>
2013-10-09 12:28:13 -0700

    remove facebook login

--- social-network-engine/app/controllers/application_controller.rb
+++ social-network-engine/app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+  def after_sign_in_path_for(resource)
+    '/reloadPosts' # <- Path you want to redirect the user to.
+  end
+
+  def after_sign_up_path_for(resource)
+    '/reloadPosts'
+  end
 end
